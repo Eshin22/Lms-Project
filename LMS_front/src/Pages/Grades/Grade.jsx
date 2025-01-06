b2fb28a1cbf43839bc3a1b548db4f0048b717352
StephanBalasuriya <kanishkabalasuriya2002@gamil.com>
2025-01-07 00:06:50 +0530

    frontend pages

---
+++ LMS_front/src/Pages/Grades/Grade.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Grade.css'
+
+function Grade() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Grade
---
+++ LMS_front/src/Pages/Logging/Logging.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Logging.css'
+
+function Logging() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Logging
