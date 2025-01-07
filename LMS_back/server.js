89fce9364d4b982dfee404ddb5a95712cd35604f
math0110 <fernandomatheesha@gmail.com>
2025-01-08 01:29:55 +0530

    changed the navbar

--- LMS_back/server.js
+++ LMS_back/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
   const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1212',
+    password: 'Matheesha@11',
     database: 'lms_project',
   });
 
--- LMS_front/src/Components/NavBar/NavBar.jsx
+++ LMS_front/src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { CiSettings } from "react-icons/ci";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 import "./NavBar.css";
 
 const NavBar = () => {
@@ -8,61 +11,53 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <ul className="navbar-menu">
-        <Link to='/'
+        <Link
+          to="/"
           className={menu === "Home" ? "active" : ""}
           onClick={() => setMenu("Home")}
         >
           Home
         </Link>
-        <Link to='/dashboard'
+        <Link
+          to="/dashboard"
           className={menu === "Dashboard" ? "active" : ""}
           onClick={() => setMenu("Dashboard")}
         >
           Dashboard
         </Link>
-        <Link to='/mycourses'
+        <Link
+          to="/mycourses"
           className={menu === "MyCourses" ? "active" : ""}
           onClick={() => setMenu("MyCourses")}
         >
           My Courses
         </Link>
-        <Link to='/profile'
-          className={menu === "Profile" ? "active" : ""}
-          onClick={() => setMenu("Profile")}
-        >
-          Profile
-        </Link>
-        <Link to='logging'
-          className={menu === "Logout" ? "active" : ""}
-          onClick={() => setMenu("Logout")}
-        >
-          Logout
-        </Link>
-        <Link to='/notification'
-          className={menu === "Notification" ? "active" : ""}
-          onClick={() => setMenu("Notification")}
-        >
-          Notification
-        </Link>
-        <Link to='/settings'
-          className={menu === "Settings" ? "active" : ""}
-          onClick={() => setMenu("Settings")}
-        >
-          Settings
-        </Link>
-        <Link to='/submission'
+        <Link
+          to="/submission"
           className={menu === "Submissions" ? "active" : ""}
           onClick={() => setMenu("Submissions")}
         >
           Submissions
         </Link>
-        <Link to='/grade'
+        <Link
+          to="/grade"
           className={menu === "Grades" ? "active" : ""}
           onClick={() => setMenu("Grades")}
         >
           Grades
         </Link>
       </ul>
+      <div className="navbar-icons">
+        <Link to="/notification" onClick={() => setMenu("Notification")}>
+          <IoIosNotificationsOutline size={24} className="icon" />
+        </Link>
+        <Link to="/settings" onClick={() => setMenu("Settings")}>
+          <CiSettings size={24} className="icon" />
+        </Link>
+        <Link to="/profile" onClick={() => setMenu("Profile")}>
+          <CgProfile size={24} className="icon" />
+        </Link>
+      </div>
     </div>
   );
 };
