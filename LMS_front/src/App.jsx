71103272dfac7d1d9f0ec5e2787eb3952c9b1739
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-01-07 22:39:02 +0530

    add routings 	modified:   LMS_front/src/App.jsx 	modified:   LMS_front/src/Components/NavBar/NavBar.jsx

--- LMS_front/src/App.jsx
+++ LMS_front/src/App.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.jsx";
+import NavBar from "./Components/NavBar/NavBar.jsx";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
+import Grade from "./Pages/Grades/Grade.jsx";
+import Logging from "./Pages/Logging/Logging.jsx";
+import Profile from "./Pages/Profile/Profile.jsx";
+
+
 
 function App() {
  
   return (
-    <div>
-        <Home />
-        {console.log("my name")}
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/grade' element={<Grade/>} />
+          <Route path='/logging' element={<Logging/>} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </div>
+
+    </BrowserRouter>
 );
 }
 
